ba78b6e151600e9a159c42f7c354272b1f479fec
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2024-07-20 15:57:20 -0300

    chore: add cors config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
+import cors from '@fastify/cors'
 import app from './app'
 import { env } from './env'
+
+app.register(cors, {
+  origin: '*',
+})
 
 app.listen({ port: Number(env.PORT) }).then(() => {
   console.log(`HTTP server running on port ${env.PORT}`)
