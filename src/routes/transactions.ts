41310ea109a1cd5948046d13abb83daa24b23354
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2024-07-20 01:58:02 -0300

    feat: add GET transactions route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,12 @@ import { knex } from '../database'
 import { createTransactionBodySchema } from '../schemas/transactions'
 
 export const transactionsRoutes = async (app: FastifyInstance) => {
+  app.get('/', async (_, reply) => {
+    const transactions = await knex('transactions').select('*')
+
+    return reply.status(200).send({ transactions })
+  })
+
   app.post('/', async (req, reply) => {
     const { amount, title, type } = createTransactionBodySchema.parse(req.body)
 
