7ef8925463980508a77d948518ab87666d76afdc
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2024-07-20 01:58:27 -0300

    feat: add GET transaction route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,13 +1,26 @@
 import { randomUUID } from 'node:crypto'
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
-import { createTransactionBodySchema } from '../schemas/transactions'
+import {
+  createTransactionBodySchema,
+  getTransactionParamsSchema,
+} from '../schemas/transactions'
 
 export const transactionsRoutes = async (app: FastifyInstance) => {
   app.get('/', async (_, reply) => {
     const transactions = await knex('transactions').select('*')
 
     return reply.status(200).send({ transactions })
+  })
+
+  app.get('/:transactionId', async (req, reply) => {
+    const { transactionId } = getTransactionParamsSchema.parse(req.params)
+
+    const transaction = await knex('transactions')
+      .where('id', transactionId)
+      .first()
+
+    return reply.status(200).send({ transaction })
   })
 
   app.post('/', async (req, reply) => {
